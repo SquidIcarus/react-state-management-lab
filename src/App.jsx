e88173e60c0be5e88e45aca3afdcf79106de315e
User Name <landonisaiah@gmail.com>
2025-07-30 10:46:13 -0700

    display list of

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@
 // 1. Create a new state variable named `team` and set the initial state to an empty array []
 // 2. Create a new state variable named `money` and set the initial state to `100`
 // 3. Create a new state variable named `zombieFighters` and set the initial state to the given array of objects of 'fighters'
+// 4. Display the list of `zombieFighters` by mapping the array into the UI of `App.jsx`
 
 import { useState } from 'react'; // imports the `useState()` hook from react
 import './App.css';
+
+// ************************** DEFINING THE APP COMPONENT ************************************* //
 
 const App = () => {
   const [team, setTeam] = useState([]); // Step 1. `team` is the state variable, `setTeam` is the setter function, `useState` is set with an empty array
@@ -93,8 +96,27 @@ const App = () => {
     },
   ]);
 
+  //*********************************** JSX THAT GETS RENDERED TO THE SCREEN **************************/
+
   return (
-    <h1>Hello world!</h1>
+    <>
+
+      <h1>Zombie Fighters</h1>
+      <ul>
+        {zombieFighters.map((fighter) => (  // uses map to iterate over the `zombieFighters` array
+          <li>
+            <img src={fighter.img} alt={fighter.name} />
+            <h3>{fighter.name}</h3>
+            <p>Price: {fighter.price}</p>
+            <p>Strength: {fighter.strength}</p>
+            <p>Agility: {fighter.agility}</p>
+            <button type="submit">Add</button>
+
+          </li>
+        ))}
+      </ul>
+
+    </>
   );
 }
 
